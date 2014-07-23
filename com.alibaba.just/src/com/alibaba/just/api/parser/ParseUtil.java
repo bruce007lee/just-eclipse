6e5a7ead9bb50ce659643bd1c7ec1fd094f6378e
bruce007lee <bruce.liz@alibaba-inc.com>
2014-07-23 09:18:47 +0800

    Add util class

---
+++ com.alibaba.just/src/com/alibaba/just/api/parser/ParseUtil.java
@@ -0,0 +1,27 @@
+package com.alibaba.just.api.parser;
+
+import java.util.List;
+
+import com.alibaba.just.api.bean.Module;
+
+public class ParseUtil {
+
+	/**
+	 * 指定的名称是否为指定模块的别名
+	 * @param moduleAlias
+	 * @param submod
+	 * @return
+	 */
+	public static boolean isMatchAlias(String moduleAlias,Module submod){
+		if(moduleAlias!=null && submod.getAlias()!=null){
+			List<String> list = submod.getAlias();
+			for(String alias:list){
+				if(moduleAlias.equals(alias)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+}
